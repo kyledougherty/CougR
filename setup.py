cf58fcd8ca8926779357a0caa1282ab9a008eeaf
Kyle Dougherty <kdougherty8@unl.edu>
2022-09-12 12:40:23 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+  name = "CougR", 
+  version = "0.0.1", 
+  url = "https://github.com/kyledougherty/CougR", 
+  author = "Kyle Dougherty", 
+  author_email = "kyle.duncan.dougherty@gmail.com",
+  packages = ["CougR"],
+  license='MIT',
+  install_requires = ["os", "ee"],
+)
