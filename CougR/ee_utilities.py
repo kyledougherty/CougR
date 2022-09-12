98f2285e6f6c9eeb5abcfcf7e22ae0baa489c35a
Kyle Dougherty <kdougherty8@unl.edu>
2022-09-12 14:51:19 -0500

    added local_raster_to_ee

--- CougR/ee_utilities.py
+++ CougR/ee_utilities.py
@@ -26,3 +26,19 @@ def local_shp_to_ee(gcs_bucket, file_name, full_file_path, asset_ID):
     "gs://" + gcs_bucket + "/" + file_name, 
     "--force"]))
 
+def local_raster_to_ee(gcs_bucket, file_name, full_file_path, asset_ID): 
+  # Define bucket to store uploaded files
+  storage_client = storage.Client()
+  bucket = storage_client.bucket(gcs_bucket)
+
+  # Instantiate a blob object in the defined GCS bucket
+  blob = bucket.blob(file_name)
+
+  # Upload local file to GCS
+  blob.upload_from_filename(full_file_path)
+
+  os.system(" ".join([
+    "earthengine upload image", 
+    "--asset_id=" + asset_ID,
+    "gs://" + gcs_bucket + "/" + file_name, 
+    "--force"]))
