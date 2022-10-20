31bda2ac4ae55620295261dcc471afbd2c5b1f1a
Kyle Dougherty <kdougherty8@unl.edu>
2022-10-20 13:45:16 -0500

    0.0.1 -> 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
   name = "CougR", 
-  version = "0.0.1", 
+  version = "0.0.2", 
   url = "https://github.com/kyledougherty/CougR", 
   author = "Kyle Dougherty", 
   author_email = "kyle.duncan.dougherty@gmail.com",
